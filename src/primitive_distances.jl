3fdef3a13d4381219f63726ed029c6640acf6138
Panagiotis Karelis <karelispanagiotis@gmail.com>
2022-04-22 15:45:51 +0300

    Changed variable names to be subscripted

--- src/primitive_distances.jl
+++ src/primitive_distances.jl
@@ -12,41 +12,41 @@ using Meshes, LinearAlgebra
 # In Information Processing Letters, no. 21, pages 55-61, 1985
 #--------------------------------------------------------------
 function segment_to_segment_dist2(A::Point, B::Point, C::Point, D::Point)
-    d1  = B - A
-    d2  = D - C
-    d12 = C - A
+    d₁  = B - A
+    d₂  = D - C
+    d₁₂ = C - A 
+ 
+    D₁ = d₁⋅d₁   
+    D₂ = d₂⋅d₂
+    R  = d₁⋅d₂
 
-    D1 = dot(d1, d1)    
-    D2 = dot(d2, d2)
-    R  = dot(d1, d2)
-
-    S1 = dot(d1, d12)
-    S2 = dot(d2, d12)
+    S₁ = d₁⋅d₁₂
+    S₂ = d₂⋅d₁₂
     
-    if D1≈0 && D2≈0
+    if D₁≈0 && D₂≈0
         t = u = 0.0f0      
-    elseif D1≈0
+    elseif D₁≈0
         t = 0.0f
-        u = clamp(-S2/D2, 0, 1)
-    elseif D2≈0
+        u = clamp(-S₂/D₂, 0, 1)
+    elseif D₂≈0
         u = 0.0f0
-        t = clamp( S1/D1, 0, 1)
-    elseif D1*D2 - R^2 ≈ 0.0
+        t = clamp( S₁/D₁, 0, 1)
+    elseif D₁*D₂ - R^2 ≈ 0.0
         t = 0.0f0
-        u = -S2/D2
+        u = -S₂/D₂
         if u<0 || u>1
             u = clamp(u, 0, 1)
-            t = clamp((u*R+S1)/D1, 0, 1)
+            t = clamp((u*R+S₁)/D₁, 0, 1)
         end
     else
-        t = clamp((S1*D2 - S2*R)/(D1*D2 - R^2), 0, 1)
-        u = (t*R - S2)/D2
+        t = clamp((S₁*D₂ - S₂*R)/(D₁*D₂ - R^2), 0, 1)
+        u = (t*R - S₂)/D₂
         if u<0 || u>1
             u = clamp(u, 0, 1)
-            t = clamp((u*R+S1)/D1, 0, 1)
+            t = clamp((u*R+S₁)/D₁, 0, 1)
         end
     end
 
-    DD = t*d1 - u*d2 - d12
+    DD = t*d₁ - u*d₂ - d₁₂
     return dot(DD, DD)
 end
