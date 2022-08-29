b006401ddcada4859f5dbed0637236dbbaa84735
Panagiotis Karelis <karelispanagiotis@gmail.com>
2022-08-29 12:42:21 +0300

    indentation fix

--- src/io.jl
+++ src/io.jl
@@ -3,21 +3,20 @@ module PlyMeshIO
 export load_meshes
 
 using Meshes
+using FileIO
 using PlyIO: load_ply
 
 function load_meshes(fname)
-  ply = load_ply(fname)
-  x = ply["vertex"]["x"]
-  y = ply["vertex"]["y"]
-  z = ply["vertex"]["z"]
-  points = Point3f.(x, y, z)
+	ply = load_ply(fname)
+	x = ply["vertex"]["x"]
+	y = ply["vertex"]["y"]
+	z = ply["vertex"]["z"]
+	points = Point3f.(x, y, z)
 
   
-  indices1 = findall(ply["face"]["obj_id"].==1)
-  indices2 = findall(ply["face"]["obj_id"].==2)
-  connec1 = [connect(Tuple(c.+1)) for c in ply["face"]["vertex_indices"][indices1]]
-  connec2 = [connect(Tuple(c.+1)) for c in ply["face"]["vertex_indices"][indices2]]
-  SimpleMesh(points, connec1), SimpleMesh(points, connec2)
-end
-
+	indices1 = findall(ply["face"]["obj_id"].==1)
+	indices2 = findall(ply["face"]["obj_id"].==2)
+	connec1 = [connect(Tuple(c.+1)) for c in ply["face"]["vertex_indices"][indices1]]
+	connec2 = [connect(Tuple(c.+1)) for c in ply["face"]["vertex_indices"][indices2]]
+	SimpleMesh(points, connec1), SimpleMesh(points, connec2)
 end
