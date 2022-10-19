8472008435de48ee8e3bce594e1fae6a3ebfd9e8
Panagiotis Karelis <karelispanagiotis@gmail.com>
2022-10-19 19:04:46 +0300

    Changed mesh load to use doubles

--- src/io.jl
+++ src/io.jl
@@ -3,11 +3,12 @@ module MeshLoader
 export load_mesh
 
 using FileIO
-using Meshes
+using GeometryBasics: GeometryBasics    # The one just for compatible
+using Meshes                            # The one real used
 using MeshBridge
 
 function load_mesh(fname)
-	old_mesh = load(fname)
+	old_mesh = load(fname; pointtype=GeometryBasics.Point{3, Float64})
 	mesh = convert(Mesh, old_mesh)
 end
 
